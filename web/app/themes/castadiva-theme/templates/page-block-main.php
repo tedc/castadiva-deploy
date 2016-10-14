36d3c8aa8147860380990a1de8a05cf20300b062
tedc <egrandinetti@gmail.com>
2016-10-14 18:39:15 +0200

    shapes

--- web/app/themes/castadiva-theme/templates/page-block-main.php
+++ web/app/themes/castadiva-theme/templates/page-block-main.php
@@ -13,7 +13,7 @@
 <ul class="grid-2 shapes-cols">
     <?php while(the_flexible_field('contenuti') || the_flexible_field('contenuti_2')) :  ?>
     <li class="col-1">
-    <?php get_template_part('templates/block', 'main-'.get_row_layout()); ?>
+    <?php include(locate_template('templates/block-main-'.get_row_layout().'.php', false, false ) ); ?>
     </li>
     <?php endwhile; ?>
 </ul>
