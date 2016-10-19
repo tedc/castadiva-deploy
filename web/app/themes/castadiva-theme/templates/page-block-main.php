b8bceb3c8c2515430b7e770dbe34c38188467591
tedc <egrandinetti@gmail.com>
2016-10-19 16:07:27 +0200

    forme

--- web/app/themes/castadiva-theme/templates/page-block-main.php
+++ web/app/themes/castadiva-theme/templates/page-block-main.php
@@ -1,4 +1,4 @@
-<?php $shape_id = 'shapes_'.$row; ?>
+<?php echo $row; $shape_id = 'shapes_'.$row; ?>
 <?php if(get_sub_field('colonne') < 2) : ?>
 <?php while(the_flexible_field('contenuti')) :  ?>
     <?php if(get_row_layout() == 'forme') : ?>
